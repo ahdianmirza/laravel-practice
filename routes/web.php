e7efa09e63378d6ebd2392ab1a9d638e837e19e4
ahdianmirza <ahdianmirza@gmail.com>
2023-07-19 17:56:08 +0700

    modelm, collection, controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,66 +16,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Ahdian Mirza",
         "email" => "ahdianmirza@gmail.my.id",
-        "image" => "farhan.jpg"
+        "image" => "ahdian.jpg"
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Ahdian Mirza",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni hic, vitae eaque ducimus aperiam obcaecati eligendi minus reiciendis perspiciatis porro. Dolorum iste pariatur explicabo tempora assumenda alias architecto magni, autem, in, laboriosam ut molestias modi? Et ullam magnam culpa veritatis fugiat in nemo obcaecati esse ea modi consequuntur sed nam, blanditiis doloremque fugit sunt accusantium voluptatibus quia minus eius dolore id quos voluptate temporibus! Doloribus aliquam iure, optio, odit vel quas numquam nihil laboriosam itaque commodi cum molestias eius iusto?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Nurin Rahayu",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti dignissimos tenetur ipsum voluptates qui commodi facilis quam quia nam. Quis vel ab quae suscipit cumque at minima sint ratione magni dolor, eveniet totam porro error laudantium ipsa repellendus provident quod itaque placeat obcaecati quo rerum et asperiores. Molestiae soluta consequatur recusandae officia reprehenderit. Corrupti non fugit dicta aspernatur ad necessitatibus eius, sapiente possimus atque nisi culpa voluptas at quod, minima est deleniti assumenda, sequi quidem. Sequi nam nesciunt molestiae blanditiis ipsa, deleniti cumque? Officia quas facere officiis id. Eum modi optio cupiditate quo facere enim maxime hic voluptatibus sunt voluptatem."
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // Halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Ahdian Mirza",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni hic, vitae eaque ducimus aperiam obcaecati eligendi minus reiciendis perspiciatis porro. Dolorum iste pariatur explicabo tempora assumenda alias architecto magni, autem, in, laboriosam ut molestias modi? Et ullam magnam culpa veritatis fugiat in nemo obcaecati esse ea modi consequuntur sed nam, blanditiis doloremque fugit sunt accusantium voluptatibus quia minus eius dolore id quos voluptate temporibus! Doloribus aliquam iure, optio, odit vel quas numquam nihil laboriosam itaque commodi cum molestias eius iusto?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Nurin Rahayu",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti dignissimos tenetur ipsum voluptates qui commodi facilis quam quia nam. Quis vel ab quae suscipit cumque at minima sint ratione magni dolor, eveniet totam porro error laudantium ipsa repellendus provident quod itaque placeat obcaecati quo rerum et asperiores. Molestiae soluta consequatur recusandae officia reprehenderit. Corrupti non fugit dicta aspernatur ad necessitatibus eius, sapiente possimus atque nisi culpa voluptas at quod, minima est deleniti assumenda, sequi quidem. Sequi nam nesciunt molestiae blanditiis ipsa, deleniti cumque? Officia quas facere officiis id. Eum modi optio cupiditate quo facere enim maxime hic voluptatibus sunt voluptatem."
-        ]
-    ];
-
-    $new_post = [];
-
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-    
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
