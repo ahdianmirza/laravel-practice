a46fc8194546a799cea685070c374bd0ed9639f1
ahdianmirza <ahdianmirza@gmail.com>
2023-07-20 20:31:36 +0700

    Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,30 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Ahdian Mirza",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni hic, vitae eaque ducimus aperiam obcaecati eligendi minus reiciendis perspiciatis porro. Dolorum iste pariatur explicabo tempora assumenda alias architecto magni, autem, in, laboriosam ut molestias modi? Et ullam magnam culpa veritatis fugiat in nemo obcaecati esse ea modi consequuntur sed nam, blanditiis doloremque fugit sunt accusantium voluptatibus quia minus eius dolore id quos voluptate temporibus! Doloribus aliquam iure, optio, odit vel quas numquam nihil laboriosam itaque commodi cum molestias eius iusto?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Nurin Rahayu",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti dignissimos tenetur ipsum voluptates qui commodi facilis quam quia nam. Quis vel ab quae suscipit cumque at minima sint ratione magni dolor, eveniet totam porro error laudantium ipsa repellendus provident quod itaque placeat obcaecati quo rerum et asperiores. Molestiae soluta consequatur recusandae officia reprehenderit. Corrupti non fugit dicta aspernatur ad necessitatibus eius, sapiente possimus atque nisi culpa voluptas at quod, minima est deleniti assumenda, sequi quidem. Sequi nam nesciunt molestiae blanditiis ipsa, deleniti cumque? Officia quas facere officiis id. Eum modi optio cupiditate quo facere enim maxime hic voluptatibus sunt voluptatem."
-        ]
-    ];
+    use HasFactory;
 
-    public static function all() {
-        return collect(self::$blog_posts);
-    }
+    // protected $fillable = [
+    //     'title',
+    //     'excerpt',
+    //     'body'
+    // ];
 
-    public static function find($slug) {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    protected $guarded = ['id'];
 }
