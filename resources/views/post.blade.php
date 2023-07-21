03fd06dc4708089bab0e3ef8d264717183231ebd
ahdianmirza <ahdianmirza@gmail.com>
2023-07-21 17:05:04 +0700

    Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,9 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
+
+        <h5>By. Ahdian Mirza Azri in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></h5>
+
         {!! $post->body !!}
     </article>
 
