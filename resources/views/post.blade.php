6b26ff836f70ca1968e5ef612d0e5d3389aeeb11
ahdianmirza <ahdianmirza@gmail.com>
2023-07-27 19:26:41 +0700

    Searching & Pagination

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,9 +6,9 @@
             <div class="col-md-8">
                 <h1 class="mb-3">{{ $post->title }}</h1>
 
-                <h5>By. <a href="/authors/{{ $post->author->username }}"
+                <h5>By. <a href="/posts?author={{ $post->author->username }}"
                         class="text-decoration-none">{{ $post->author->name }}</a> in
-                    <a href="/categories/{{ $post->category->slug }}"
+                    <a href="/posts?category={{ $post->category->slug }}"
                         class="text-decoration-none">{{ $post->category->name }}</a>
                 </h5>
 
